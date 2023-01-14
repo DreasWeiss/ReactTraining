edb41c12f479019cb0f28179289a5165d38c9ab7
DreasWeiss <dreasweiss@gmail.com>
2023-01-14 17:55:21 +0300

    counter logic for pushing btns

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import './index.scss';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const onClickPlus = () => {
+    setCount(count + 1);
+  };
+
+  const onClickMinus = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
