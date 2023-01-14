9d7394b78d68d2261ac6f7c958f09e7f3528f382
DreasWeiss <dreasweiss@gmail.com>
2023-01-14 18:07:02 +0300

    modal layout

--- src/App.js
+++ src/App.js
@@ -14,12 +14,16 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button onClick={onClickMinus} className="minus">- Минус</button>
-        <button onClick={onClickPlus} className="plus">Плюс +</button>
-      </div>
+      <button className="open-modal-btn">✨ Открыть окно</button>
+      {/* <div className="overlay">
+        <div className="modal">
+          <svg height="200" viewBox="0 0 200 200" width="200">
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        </div>
+      </div> */}
     </div>
   );
 }
