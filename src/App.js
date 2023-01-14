92ac02f22280b11a8f1d9cba3b4f61c90d000330
DreasWeiss <dreasweiss@gmail.com>
2023-01-14 17:49:26 +0300

    counter layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './index.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>0</h1>
+        <button className="minus">- Минус</button>
+        <button className="plus">Плюс +</button>
+      </div>
     </div>
   );
 }
