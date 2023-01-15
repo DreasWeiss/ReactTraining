1bce4ca57046751d24f175ba8d59bf9f61f145e6
DreasWeiss <dreasweiss@gmail.com>
2023-01-15 15:31:44 +0300

    modal second way with animatin and adding class

--- src/App.js
+++ src/App.js
@@ -6,18 +6,16 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={ () => setOpen(true) } className="open-modal-btn">✨ Открыть окно</button>
-      { open && (
-        <div className="overlay">
+      <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
+      <div className={`overlay animated ${ open ? 'show' : ''}`}>
         <div className="modal">
-          <svg onClick={ () => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+          <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
         </div>
       </div>
-      )}
     </div>
   );
 }
