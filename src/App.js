7ef18cacc383459254d519e09ce1aab97e9e666b
DreasWeiss <dreasweiss@gmail.com>
2023-01-15 15:18:01 +0300

    modal first way (without animations) -fixed bag with close btn

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
       { open && (
         <div className="overlay">
         <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
+          <svg onClick={ () => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
             <title />
-            <path onClick={ () => setOpen(false)} d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
         </div>
