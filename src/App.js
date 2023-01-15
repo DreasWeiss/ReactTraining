aab7eb0153a0e60a347bd2df67d8e24f27c1741e
DreasWeiss <dreasweiss@gmail.com>
2023-01-15 15:57:18 +0300

    modal second way 	-added children for dinamic render inside

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import './index.scss';
 
-const Modal = ({open, setOpen }) => (
+const Modal = ({open, setOpen, children }) => (
   <div className={`overlay animated ${ open ? 'show' : ''}`}>
         <div className="modal">
           <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          {children}
         </div>
       </div>
 );
@@ -19,7 +19,9 @@ function App() {
   return (
     <div className="App">
       <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
-      <Modal open={open} setOpen={setOpen}/>
+      <Modal open={open} setOpen={setOpen}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+      </Modal>
     </div>
   );
 }
