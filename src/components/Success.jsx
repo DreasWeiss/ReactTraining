3481d2141582563fc619f916b684ee40d13973dc
DreasWeiss <dreasweiss@gmail.com>
2023-01-18 21:36:52 +0300

    users invites list demo

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export const Success = ({ count }) => {
   return (
-    <div class="success-block">
+    <div className="success-block">
       <img src="/assets/success.svg" alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
-      <button className="send-invite-btn">Назад</button>
+      <button onClick={() => window.location.reload()} className="send-invite-btn">Назад</button>
     </div>
   );
 };
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading }) => {
+export const Users = (
+  { items, 
+    isLoading, 
+    searchValue, 
+    onChangeSerchValue,
+    onClickInvite,
+    invites,
+    onClickSendInvites
+  }
+  ) => {
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input value={searchValue} onChange={onChangeSerchValue} type="text" placeholder="Найти пользователя..." />
       </div>
-      {isLoading ? (
+      { isLoading ? (
         <div className="skeleton-list">
           <Skeleton />
           <Skeleton />
@@ -19,10 +28,22 @@ export const Users = ({ items, isLoading }) => {
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {
+            items.filter((obj) => {
+              const fullname = (`${obj.first_name} ${obj.last_name}`).toLowerCase();
+              return fullname.includes(searchValue.toLowerCase()) || obj.email.includes(searchValue.toLowerCase());
+            }).map(obj => (
+              <User 
+              onClickInvite={onClickInvite}
+              isInvited={invites.includes(obj.id)}
+              key={obj.id} 
+              {...obj}/>
+            ))
+          }
+        
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      <button onClick={onClickSendInvites} className="send-invite-btn">Отправить приглашение</button>
     </>
   );
 };
