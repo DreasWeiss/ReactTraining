a221500d5b9f40a46624fe5d7d649a1218ff732a
DreasWeiss <dreasweiss@gmail.com>
2023-01-18 21:52:08 +0300

    currency-convertor

--- src/App.js
+++ src/App.js
@@ -1,61 +1,12 @@
 import React from 'react';
+import { Block } from './Block';
 import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
-
-// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  const [users, setUsers] = React.useState([]);
-  const [invites, setInvites] = React.useState([]);
-  const [isLoading, setLoading] = React.useState(true);
-  const [success, setSuccess] = React.useState(false);
-  const [searchValue, setSearchValue] = React.useState('');
-
-  React.useEffect(() => {
-    fetch('https://reqres.in/api/users')
-      .then((res) => res.json())
-      .then((json) => {
-        setUsers(json.data);
-      }).catch(err => {
-        console.warn(err);
-        alert('error with users data');
-      }).finally(() => setLoading(false));
-  }, []);
-
-  const onChangeSerchValue = (event) => {
-    setSearchValue(event.target.value);
-  };
-
-  const onClickInvite = (id) => {
-    if (invites.includes(id)) {
-      setInvites(prev => prev.filter(_id => _id !== id));
-    } else {
-      setInvites(prev => [...prev, id]);
-    }
-  };
-
-  const onClickSendInvites = () => {
-    setSuccess(true);
-  };
-
   return (
     <div className="App">
-      {
-        success ? (
-          <Success count={invites.length}/>
-        ) : (
-          <Users
-            items={users}
-            isLoading={isLoading}
-            onChangeSerchValue={onChangeSerchValue}
-            searchValue={searchValue}
-            invites={invites}
-            onClickInvite={onClickInvite}
-            onClickSendInvites={onClickSendInvites}
-          />
-        )
-      }
+      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block value={0} currency="USD" />
     </div>
   );
 }
